b59b3f92c5a65240c989f35699eb50bf00ddf112
megaman3427 <megaman3427@gmail.com>
2024-05-27 01:16:10 -0700

    Editing HTML

---
+++ js/homepage.js
@@ -0,0 +1,18 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+});
+
+// Close the menu if clicked outside
+window.onclick = function(event) {
+    if (!event.target.matches('#menu-toggle')) {
+        var menu = document.getElementById('menu');
+        if (menu.style.display === 'block') {
+            menu.style.display = 'none';
+        }
+    }
+}
