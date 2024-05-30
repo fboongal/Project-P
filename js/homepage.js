cbc50a662b3a73aae039a50d6a458eb10556f164
Franchesca <fboongal@ucsc.edu>
2024-05-30 01:35:46 -0700

    added temporary buttons and functions(and accompanying css) to direct to specific bathroom locations, renamed old files and put into .old-files folder

--- js/homepage.js
+++ js/homepage.js
@@ -1,21 +1,8 @@
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
-    }
-});
 
-// Close the menu if clicked outside
-window.onclick = function(event) {
-    if (!event.target.matches('#menu-toggle')) {
-        var menu = document.getElementById('menu');
-        if (menu.style.display === 'block') {
-            menu.style.display = 'none';
-        }
-    }
-}
-
-
-
+$('button').click(function(){
+    let tabID = $(this).data('location');
+    $('button').removeClass('active');
+    $(this).addClass('active');
+    $('.tab-pane').removeClass('active');
+    $('[data-location-content="' + tabID + '"]').addClass('active')
+})
