2410d885e8a77439aeee1a68fcca53fac3c924dc
nathanyu8th <nathanyu8th@gmail.com>
2024-05-30 17:10:44 -0700

    bathroom

--- js/homepage.js
+++ js/homepage.js
@@ -1,8 +1,18 @@
-
-$('button').click(function(){
-    let tabID = $(this).data('location');
-    $('button').removeClass('active');
-    $(this).addClass('active');
-    $('.tab-pane').removeClass('active');
-    $('[data-location-content="' + tabID + '"]').addClass('active')
+//change the id of the button to general_button
+$('.general_button').click(function(){
+  let tabID = $(this).data('location');
+  $('.general_button').removeClass('active');
+  $(this).addClass('active');
+  $('.tab-pane').removeClass('active');
+  $('[data-location-content="' + tabID + '"]').addClass('active');
 })
+
+//using buttons for specific_button
+$('.specific_button').click(function(){
+  let tabID = $(this).data('location');
+  $('.specific_button').removeClass('active');
+  $(this).addClass('active');
+  $('.tab-pane').removeClass('active');
+  $('[data-location-content="' + tabID + '"]').addClass('active');
+})
+
