7e99d15de411b3238c73b39f1667a415d565d845
Franchesca <fboongal@ucsc.edu>
2024-05-30 19:43:51 -0700

    created togglable expand buttons

--- js/homepage.js
+++ js/homepage.js
@@ -7,12 +7,39 @@ $('.general_button').click(function(){
   $('[data-location-content="' + tabID + '"]').addClass('active');
 })
 
+let TAOw = 0;
+let TAOm = 0;
+
 //using buttons for specific_button
-$('.specific_button').click(function(){
+$('.TAO-Women').click(function(){
   let tabID = $(this).data('location');
-  $('.specific_button').removeClass('active');
+  $('.TAO-Women').removeClass('active');
   $(this).addClass('active');
-  $('.tab-pane').removeClass('active');
-  $('[data-location-content="' + tabID + '"]').addClass('active');
+  if (TAOw == 0) {
+    $('.exTAO-Women').removeClass('hidden');
+    $('[data-location-content="' + tabID + '"]').addClass('active');
+    TAOw = 1;
+  } else {
+    $('.exTAO-Women').addClass('hidden');
+    $('[data-location-content="' + tabID + '"]').removeClass('active');
+    TAOw = 0;
+  }
 })
+
+$('.TAO-Men').click(function(){
+  let tabID = $(this).data('location');
+  $('.TAO-Men').removeClass('active');
+  $(this).addClass('active');
+  if (TAOm == 0) {
+    $('.exTAO-Men').removeClass('hidden');
+    $('[data-location-content="' + tabID + '"]').addClass('active');
+    TAOm = 1;
+  } else {
+    $('.exTAO-Men').addClass('hidden');
+    $('[data-location-content="' + tabID + '"]').removeClass('active');
+    TAOm = 0;
+  }
+})
+
+
 
